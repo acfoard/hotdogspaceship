11757b789348fb09527bc799f38f0721fcff50d2
ecthyr <arreisnahtan@gmail.com>
2019-03-08 10:42:18 -0500

    includes react

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
