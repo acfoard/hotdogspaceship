e08d93c89ccdd27f71fdc9ae5a520c56f47e997e
ecthyr <arreisnahtan@gmail.com>
2019-03-08 14:39:15 -0500

    make sounds

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from './components/Game/game'
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Welcome HotDogSpaceShipTeam</h1>
+        <Game/>
       </div>
     )
   }
---
+++ client/src/components/Game/game.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class Game extends React.Component {
+  state = {
+    red: { sound: 392
+
+    },
+    blue: { sound: 494
+
+    },
+    green: { sound: 587
+
+    },
+    violet: { sound: 659
+
+    }
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    console.log('clicked');
+    this.playSound(event)
+    
+  }
+
+  playSound(event) {
+    console.log('sound should be played');
+    console.log(`${event.target.value}`)
+    const audioContext = new AudioContext();
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    
+
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(`${event.target.value}`, audioContext.currentTime)
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 0.5);
+  }
+
+  render () {
+    return(
+      <div>
+        <h2>Would you like to play a game?</h2>
+        <button onClick={this.handleClick} value="392" className="red">Red</button>
+        <button onClick={this.handleClick} value="494" className="blue">Blue</button>
+        <button onClick={this.handleClick} value="587" className="green">Green</button>
+        <button onClick={this.handleClick} value="659" className="violet">Violet</button>
+      </div>
+    )
+
+  }
+}
+
+export default Game;
