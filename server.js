9e40cfb5eb2b951d84f5ccc9872d99fa111990b4
ecthyr <arreisnahtan@gmail.com>
2019-03-08 10:46:28 -0500

    includes production env

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 
 const db = require('./models');
