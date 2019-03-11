9f662a790a19cdc0fe9d4b3495d2892ea916abef
acfoard <acfoard@gmail.com>
2019-03-11 14:16:47 -0400

    games page basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/LoginPage'
+import GameList from './components/GamePage'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Login/>
+        <GameList />
       </div>
     )
   }
